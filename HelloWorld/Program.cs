e0d1b6ab9b1c278b582571f48e18636d6de760b9
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-07-28 14:10:02 +0530

    [saiVijay] empty

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to visual studio");
+            int a = 4, b = 8;
+            Console.Write(a + b);
+            Console.ReadLine();
+        }
+    }
+}
